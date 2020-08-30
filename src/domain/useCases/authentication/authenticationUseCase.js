793e18f8562f65fe93380da36c61bf1bd04e193e
Johan Duran <johan.parra_96@hotmail.com>
2020-08-30 18:39:03 -0500

    fix: agregando try/catch al caso de uso de autenticacion

--- src/domain/useCases/authentication/authenticationUseCase.js
+++ src/domain/useCases/authentication/authenticationUseCase.js
@@ -1,26 +1,29 @@
-'use strict'
+"use strict";
 
-class AuthenticationUseCase{
-    
-    constructor({ userRepository }={}){
-        this.userRepository = userRepository;
-    }
+class AuthenticationUseCase {
+  constructor({ userRepository } = {}) {
+    this.userRepository = userRepository;
+  }
 
-    async authentication(correo,clave,getHash){
-        if(!correo){
-            throw new Error('La propiedad correo debe ser obligatoria');
-        }
-        if(!clave){
-            throw new Error('La propiedad clave debe ser obligatoria');
-        }
-        if(!getHash){
-            throw new Error('La propiedad getHash debe ser obligatoria');
-        }
-        const usuario = await this.userRepository.getUserByEmail(correo);
-        if(!usuario){
-            return null;
-        }
+  async authentication(correo, clave, getHash) {
+    try {
+      if (!correo) {
+        throw new Error("La propiedad correo debe ser obligatoria");
+      }
+      if (!clave) {
+        throw new Error("La propiedad clave debe ser obligatoria");
+      }
+      if (!getHash) {
+        throw new Error("La propiedad getHash debe ser obligatoria");
+      }
+      const usuario = await this.userRepository.getUserByEmail(correo);
+      if (!usuario) {
+        return null;
+      }
+    } catch (error) {
+        throw error;
     }
+  }
 }
 
-module.exports = AuthenticationUseCase
+module.exports = AuthenticationUseCase;

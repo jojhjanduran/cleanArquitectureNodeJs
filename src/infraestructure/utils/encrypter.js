99dc61f92a5ee630d553ee6768be69f59f728526
Johan Duran <johan.parra_96@hotmail.com>
2020-09-05 20:29:53 -0500

    feat: install bcrypt dependency

---
+++ src/infraestructure/utils/encrypter.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcrypt');
+class Encrypter{
+    async compare(value,hash){
+        if(!value){
+            throw new Error('El parametro value es obligatorio');
+        }
+        if(!hash){
+            throw new Error('El parametro hash es obligatorio');
+        }
+        const isValid = await bcrypt.compare(value,hash);
+        return isValid;
+    }
+}
+module.exports = Encrypter;
